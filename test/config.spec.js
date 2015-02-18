6f77a04aa20367660c614ed9afc90835226956d0
Russell Matney <russell.matney@gmail.com>
2015-02-17 21:52:39 -0500

    test 2 and implementation

--- test/config.spec.js
+++ test/config.spec.js
@@ -6,4 +6,10 @@ describe('Config', function() {
   it('should exist', function() {
     expect(Config).to.exist;
   });
+
+  describe('init', function() {
+    it('should initialize itself cleanly', function() {
+      expect(Config()).to.be.instanceof(Object);
+    });
+  });
 });
