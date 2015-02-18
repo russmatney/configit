3f58ef9390b05bd271c50b4fed6342aac4383026
Russell Matney <russell.matney@gmail.com>
2015-02-17 21:42:45 -0500

    npm init and first test

---
+++ test/config.spec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+var Config = require('../index.js');
+
+describe('Config', function() {
+  it('should exist', function() {
+    expect(Config).to.exist();
+  });
+});
